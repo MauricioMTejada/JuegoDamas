9d96e44bf1a0f1914890f401e7906c596455f76b
MauricioTejada <mauriciotejada14@gmail.com>
2023-04-06 22:21:45 -0300

    nada

---
+++ temporal.js
@@ -0,0 +1,22 @@
+/* ¡Escribe tus comandos en este archivo! */
+
+const ejercicio02 = 'SELECT * FROM movies WHERE duration < 90;';
+
+const ejercicio03 = 'SELECT * FROM movies WHERE year BETWEEN 1930 AND 1940;';
+
+const ejercicio04 = 'SELECT * FROM movies WHERE title LIKE '%til%';';
+
+const ejercicio05 = 'SELECT * FROM movies  WHERE cardinality(actors)=1;';
+
+const ejercicio06 = '';
+
+const ejercicio07 = '';
+
+module.exports = {
+   ejercicio02,
+   ejercicio03,
+   ejercicio04,
+   ejercicio05,
+   ejercicio06,
+   ejercicio07,
+};
